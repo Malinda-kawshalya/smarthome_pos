b031afb5577a09561daae61f785f59a31999b86c
Malinda Kawshalya <malindakawshalya@gmail.com>
2025-06-24 23:50:01 +0530

    change root file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/dashboard';
-import StockManagement from './pages/stockManagement'; 
+import StockManagement from './pages/stockManagement';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Dashboard />
-      <StockManagement />
+      <Router>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/stock" element={<StockManagement />} />
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
