eba79343eefc68181e4bf8c99438015b9b67dfbc
Malinda Kawshalya <malindakawshalya@gmail.com>
2025-07-01 23:05:37 +0530

    change the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/dashboard';
 import StockManagement from './pages/stockManagement';
-import Header from './components/Header';
-import Sidebar from './components/Sidebar';
 import EmployeeManagement from './pages/employeeManagment';
 import POS from './pages/posInterface';
 import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import Analytics from './pages/Analytics';
-import orders from './pages/Orders';
-import './App.css';
 import Orders from './pages/Orders';
+import Products from './pages/Products';
+import './App.css';
 
 function App() {
   return (
@@ -20,19 +18,14 @@ function App() {
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/stock" element={<StockManagement />} />
-          <Route path="/" element={<Navigate to="/dashboard" replace />} />
-          <Route path="/header" element={<Header />} />
-          <Route path="/Sidebar" element={<Sidebar />} />
+          <Route path="/products" element={<Products />} />
           <Route path="/employees" element={<EmployeeManagement />} />
           <Route path="/pos" element={<POS />} />
+          <Route path="/analytics" element={<Analytics />} />
+          <Route path="/orders" element={<Orders />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignUpPage />} />
-          <Route path='/analytics' element={<Analytics />} />
-          <Route path='/orders' element={<Orders />} />
-
-          {/* Add more routes as needed */}
-          
-
+          <Route path="/" element={<Navigate to="/dashboard" replace />} />
         </Routes>
       </Router>
     </div>
